cb8a150443b04c733fd42c21fa63f11666250b85
Nina Kim <ninankkim@gmail.com>
2021-10-31 17:41:59 -0400

    dragon scanner exercise

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+// classes hold variables and methods
+public class Main {
+public static void main(String[] args) {
+        Scanner getInput = new Scanner(System.in);
+        String input = getInput.nextLine();
+        System.out.println(input);
+        }
+}
